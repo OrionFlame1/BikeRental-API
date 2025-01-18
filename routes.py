d66270e646e5c1ea93be26b62eac8ef8f20f893e
adito <aditoma123@yahoo.com>
2025-01-18 22:14:28 +0200

    added more routes

--- routes.py
+++ routes.py
@@ -1,15 +1,10 @@
-from flask import Blueprint, jsonify
-from db import get_db_connection, get
+from flask import Blueprint, jsonify, request
+from db import get, post
 
 routes = Blueprint('routes', __name__)
 
 
-@routes.route('/get/<tablename>', methods=['GET'])
-def getUsers(tablename):
-    return jsonify(get(f"SELECT * FROM {tablename}"))
-
-
-@routes.route('/get-available-bikes', methods=['GET']) # BROKEN SQL?
+@routes.route('/getAllAvailableBikes', methods=['GET'])  # BROKEN SQL?
 def getAvailableBikes():
     return jsonify(get(
         "SELECT b.BikeID, b.BikeType, b.PurchaseDate, b.BikeProducer, b.BikeDefaultTarif " +
@@ -21,15 +16,42 @@ def getAvailableBikes():
     ))
 
 
-# @routes.route('/insert', methods=['POST'])
-# def insert_data():
-#     connection = get_db_connection()
-#     cursor = connection.cursor()
-#
-#     # Assuming you're inserting a record with two fields (adjust based on your table)
-#     cursor.execute('INSERT INTO your_table (column1, column2) VALUES (%s, %s)', ('value1', 'value2'))
-#     connection.commit()
-#     cursor.close()
-#     connection.close()
-#
-#     return jsonify({"message": "Data inserted successfully!"})
+@routes.route('/getAllBikes', methods=['GET'])
+def getAllBikes():
+    return jsonify(get(f"SELECT * FROM bikes"))
+
+# @routes.route('/checkAvailability', methods=['GET'])
+# def checkAvailability():
+#     return jsonify(get(f"SELECT * FROM bikes"))
+
+
+@routes.route('/getUserRentals', methods=['GET'])
+def getUserRentals():
+    return jsonify(get(f"SELECT * FROM rentals WHERE userid = {request.args.get("user_id")}"))
+
+
+@routes.route('/startRental', methods=['POST'])
+def startRental():
+    p = request.get_json()
+    user = "User1"
+    return jsonify(post(
+        f"INSERT INTO rentals (userid, bikeid, starttime, startlocation) "
+        f"VALUES ({user}, {p["bike_id"]}, LOCALTIMESTAMP, {p["start_location"]}"))
+
+@routes.route('/stopRental', methods=['POST'])
+def stopRental():
+    p = request.get_json()
+
+    # finishing rental
+    post(f"INSERT INTO rentals (endtime, endlocation) "
+         f"VALUES (LOCALTIMESTAMP, {p["location_end"]}) "
+         f"WHERE rentalid = {p["rental_id"]}")
+
+    # TODO: FIX CALCULATION
+    amount = 0
+
+    # adding payment details, card number hint hardcoded or get from request?
+    post(f"INSERT INTO payments (rentalid, paymentdate, amount, paymentmethod, currency, cardnumberhint)"
+         f"VALUES ({p["rental_id"]}, LOCALTIMESTAMP, {amount}, \"Card\", \"EURO\", \"1234\")")
+
+
