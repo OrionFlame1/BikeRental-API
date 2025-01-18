13ba0bb55f8d926e27c0fb0429bd23c3970d53f6
adito <aditoma123@yahoo.com>
2025-01-18 22:15:10 +0200

    added post function for insertions

--- db.py
+++ db.py
@@ -19,7 +19,30 @@ def get(sql):
     connection = get_db_connection()
     cursor = connection.cursor()
     cursor.execute(sql)
-    rows = cursor.fetchall()
+    rows = []
+    for buff in cursor:
+        row = {}
+        c = 0
+        for col in cursor.description:
+            row.update({str(col[0]): buff[c]})
+            c += 1
+        rows.append(row)
+    # rows = cursor.fetchall()
     cursor.close()
     connection.close()
     return rows
+
+def post(sql):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        # Assuming you're inserting a record with two fields (adjust based on your table)
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        return {"message" : e}
+    else:
+        return {"message" : "Data inserted successfully"}
